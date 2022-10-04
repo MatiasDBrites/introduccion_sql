3e1bb40709982dbce7e14fa7700453d5e57d362c
MatiasDBrites <matias.brites@gmail.com>
2022-10-04 12:11:02 -0500

    Introduccion a sql

---
+++ vocalista.sql
@@ -0,0 +1,13 @@
+create table vocalista (
+idpersona int not null,
+nombre varchar(20),
+años int,
+estilo varchar(20),
+agrupacion varchar(20)
+);
+
+insert into vocalista values ('1', 'Ozzy_Osbourn', '73', 'Heavy_metal', 'Black_Sabbath');
+insert into vocalista values ('2', 'Ronnie_James_Dio', '67', 'Heavy_metal', 'Dio');
+insert into vocalista values ('3', 'Rob_Halford', '71', 'Heavy_metal', 'Judas_Priest');
+insert into vocalista values ('4', 'Bon_Scott', '33', 'Heavy_metal', 'Ac_Dc');
+insert into vocalista values ('5', 'Bruce_Dickinson', '64', 'Heavy_metal', 'Iron_Maiden');
