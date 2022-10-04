92a368a2466aeb7ad0ced407fd0b0b4ce094a85a
Matias D Brites <104856701+MatiasDBrites@users.noreply.github.com>
2022-10-04 13:29:04 -0500

    Update vocalista.sql

--- vocalista.sql
+++ vocalista.sql
@@ -11,3 +11,8 @@ insert into vocalista values ('2', 'Ronnie_James_Dio', '67', 'Heavy_metal', 'Dio
 insert into vocalista values ('3', 'Rob_Halford', '71', 'Heavy_metal', 'Judas_Priest');
 insert into vocalista values ('4', 'Bon_Scott', '33', 'Heavy_metal', 'Ac_Dc');
 insert into vocalista values ('5', 'Bruce_Dickinson', '64', 'Heavy_metal', 'Iron_Maiden');
+
+select * from vocalista;
+
+select i.nombre, i.estilo  from vocalista i where i.años < '68';
+
